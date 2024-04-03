aefc9257c2e26223a1cd5e1f452b14e034520edb
zeru0429 <mihiretutigistu@gmail.com>
2024-04-03 08:29:11 +0300

    try to fix

--- src/util/sms.ts
+++ src/util/sms.ts
@@ -1,14 +1,14 @@
-import { ACCOUNT_ID, AUTH_TOKEN, PHONE } from "../config/secrets.js";
-import twilio from 'twilio';
+// import { ACCOUNT_ID, AUTH_TOKEN, PHONE } from "../config/secrets.js";
+// import twilio from 'twilio';
 
-const client = twilio(ACCOUNT_ID, AUTH_TOKEN);
+// const client = twilio(ACCOUNT_ID, AUTH_TOKEN);
 
-export const sendSMS = (sendTo: string, message: string) => {
-  client.messages.create({
-    body: message,
-    to: sendTo,
-    from: PHONE,
-  })
-  .then((message: any) => console.log(message.sid))
-  .catch((error: any) => console.error(error)); // Add error handling
-};
+// export const sendSMS = (sendTo: string, message: string) => {
+//   client.messages.create({
+//     body: message,
+//     to: sendTo,
+//     from: PHONE,
+//   })
+//   .then((message: any) => console.log(message.sid))
+//   .catch((error: any) => console.error(error)); // Add error handling
+// };
